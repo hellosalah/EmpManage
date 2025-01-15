af7033a7149f1134060f678e6692c47ac0c17839
Salah <intothenightsky004@gmail.com>
2025-01-15 13:49:43 +0100

    Set up database schema
    Set up database schema

---
+++ src/main/java/com/example/EmpManage/model/Employee.java
@@ -0,0 +1,39 @@
+package com.example.EmpManage.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String jobTitle;
+
+    private String department;
+
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private EmploymentStatus employmentStatus;
+
+    private int contactInformation;
+
+    private String address;
+}
+
+enum EmploymentStatus {
+    FULL_TIME, INTERN, FREELANCER
+}
