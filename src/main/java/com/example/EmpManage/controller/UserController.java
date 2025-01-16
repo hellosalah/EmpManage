0874296b5d8d51410fb1b660468d42592d4ef8e0
Salah <intothenightsky004@gmail.com>
2025-01-16 15:00:16 +0100

    Added user controller

---
+++ src/main/java/com/example/EmpManage/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.EmpManage.controller;
+
+import com.example.EmpManage.model.Role;
+import com.example.EmpManage.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class UserController {
+
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public Role authenticate(@RequestParam String username, @RequestParam String password) {
+        return userService.authenticate(username, password);
+    }
+}
