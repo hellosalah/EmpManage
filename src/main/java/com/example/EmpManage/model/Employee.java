22c528e6910d3c6a3de32e7afb436558ac61f625
Salah <intothenightsky004@gmail.com>
2025-01-16 14:06:39 +0100

    Added getters and setters

--- src/main/java/com/example/EmpManage/model/Employee.java
+++ src/main/java/com/example/EmpManage/model/Employee.java
@@ -35,9 +35,91 @@ public class Employee {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public EmploymentStatus getEmploymentStatus() {
+        return employmentStatus;
+    }
+
+    public void setEmploymentStatus(EmploymentStatus employmentStatus) {
+        this.employmentStatus = employmentStatus;
+    }
+
+    public int getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(int contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
 
 enum EmploymentStatus {
     FULL_TIME, INTERN, FREELANCER
 }
+
+
 
