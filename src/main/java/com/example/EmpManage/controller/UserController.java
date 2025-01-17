ab7603d50fad69b91aa447355d3e8acdf95c36b4
Salah <intothenightsky004@gmail.com>
2025-01-17 13:03:48 +0100

    Added annotations

--- src/main/java/com/example/EmpManage/controller/UserController.java
+++ src/main/java/com/example/EmpManage/controller/UserController.java
@@ -3,8 +3,12 @@ package com.example.EmpManage.controller;
 import com.example.EmpManage.model.Role;
 import com.example.EmpManage.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping
 public class UserController {
 
     private final UserService userService;
--- src/main/java/com/example/EmpManage/service/UserService.java
+++ src/main/java/com/example/EmpManage/service/UserService.java
@@ -1,10 +1,13 @@
 package com.example.EmpManage.service;
 
+import com.example.EmpManage.config.UserSession;
 import com.example.EmpManage.model.Role;
 import com.example.EmpManage.model.User;
 import com.example.EmpManage.repository.UserRepository;
 import com.example.EmpManage.model.Employee;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     private final UserRepository userRepository;
 
@@ -17,6 +20,7 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("Invalid username or password"));
 
         if (user.getPassword().equals(password)) {
+            UserSession.getInstance().setCurrentUser(user);
             return user.getRole();
         } else {
             throw new IllegalArgumentException("Invalid username or password");
