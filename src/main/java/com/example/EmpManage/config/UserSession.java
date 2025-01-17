b427436a416595babf427f77e78e891447e1b492
Salah <intothenightsky004@gmail.com>
2025-01-17 13:03:35 +0100

    Created UserSession class

---
+++ src/main/java/com/example/EmpManage/config/UserSession.java
@@ -0,0 +1,29 @@
+package com.example.EmpManage.config;
+
+import com.example.EmpManage.model.User;
+
+public class UserSession {
+    private static UserSession instance;
+    private User currentUser;
+
+    private UserSession() {}
+
+    public static UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void clearSession() {
+        currentUser = null;
+    }
+}
